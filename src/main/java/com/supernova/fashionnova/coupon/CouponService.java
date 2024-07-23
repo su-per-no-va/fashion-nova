a85d4169472398373dfedc7bf54818832445c11d
equis3351 <equis3351@naver.com>
2024-07-23 16:21:27 +0900

    🎉 FEAT(쿠폰): 쿠폰 조회 기능
    보유 쿠폰 조회, 쿠폰 내역 조회

--- src/main/java/com/supernova/fashionnova/coupon/CouponService.java
+++ src/main/java/com/supernova/fashionnova/coupon/CouponService.java
@@ -1,9 +1,34 @@
 package com.supernova.fashionnova.coupon;
 
+import com.supernova.fashionnova.coupon.dto.CouponResponseDto;
+import com.supernova.fashionnova.user.User;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CouponService {
+
+    private final CouponRepository couponRepository;
+
+    public List<CouponResponseDto> getCouponList(User user) {
+
+        List<Coupon> coupons = couponRepository.findByUserAndIsCouponUsed(user, false);
+
+        return coupons.stream()
+            .map(CouponResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
+    public List<CouponResponseDto> getUsedCouponList(User user) {
+
+        List<Coupon> coupons = couponRepository.findByUserAndIsCouponUsed(user, true);
+
+        return coupons.stream()
+            .map(CouponResponseDto::new)
+            .collect(Collectors.toList());
+    }
+
 }
--- src/main/java/com/supernova/fashionnova/coupon/dto/CouponResponseDto.java
+++ src/main/java/com/supernova/fashionnova/coupon/dto/CouponResponseDto.java
@@ -1,9 +1,31 @@
 package com.supernova.fashionnova.coupon.dto;
 
+import com.supernova.fashionnova.coupon.Coupon;
+import com.supernova.fashionnova.coupon.CouponType;
+import com.supernova.fashionnova.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class CouponResponseDto {
+
+    private final String name;
+    private final Date period;
+    private final String sale;
+    private final CouponType type;
+
+    public CouponResponseDto(Coupon coupon) {
+        this.name = coupon.getName();
+        this.period = coupon.getPeriod();
+        this.sale = coupon.getSale();
+        this.type = coupon.getType();
+    }
+
 }
