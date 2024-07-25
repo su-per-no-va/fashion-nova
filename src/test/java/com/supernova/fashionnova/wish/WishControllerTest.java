d011f9f98129a824fd08482faa034fc541843253
equis3351 <equis3351@naver.com>
2024-07-25 12:11:09 +0900

    🍻 TEST(위시): 위시리스트 테스트 init

---
+++ src/test/java/com/supernova/fashionnova/wish/WishControllerTest.java
@@ -0,0 +1,97 @@
+package com.supernova.fashionnova.wish;
+
+import static org.mockito.BDDMockito.given;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.supernova.fashionnova.security.UserDetailsImpl;
+import com.supernova.fashionnova.user.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(WishController.class)
+class WishControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String baseUrl = "/wishlists";
+    UserDetailsImpl userDetails = Mockito.mock(UserDetailsImpl.class);
+
+    @MockBean
+    private WishService wishService;
+
+    @BeforeEach
+    void setUp() {
+
+        // Given a mock UserDetailsImpl
+        given(userDetails.getUsername()).willReturn("user");
+        given(userDetails.getUser()).willReturn(new User(
+            "testUSer",
+            "Test1234!@",
+            "테스트유저",
+            "test@gmail.com",
+            "010-1234-5678"
+        ));
+
+        // Set the security context
+        SecurityContextHolder.setContext(new SecurityContextImpl());
+        SecurityContextHolder.getContext().setAuthentication(
+            new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities()));
+
+    }
+
+    @Test
+    void addWish() {
+
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    void getWishProductList() {
+
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    void deleteWish() {
+
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+
+}
---
+++ src/test/java/com/supernova/fashionnova/wish/WishServiceTest.java
@@ -0,0 +1,60 @@
+package com.supernova.fashionnova.wish;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class WishServiceTest {
+
+    @Mock
+    private WishRepository wishRepository;
+
+    @InjectMocks
+    private WishService wishService;
+
+    @Test
+    void addWish() {
+
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    void getWishProductList() {
+
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+
+    @Test
+    void deleteWish() {
+
+        // given
+
+
+        // when
+
+
+        // then
+
+
+    }
+
+}
