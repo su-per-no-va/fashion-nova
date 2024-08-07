db3437dcbfc2828835528229863bcf3ee9793441
secondJW <162002107+secondJW@users.noreply.github.com>
2024-08-07 18:18:40 +0900

    🎉 FEAT(재고 메서드): 환불 성공 후 재고 돌려놓기 메서드
    환불 성공 후 재고 돌려놓기 메서드 추가

--- src/main/java/com/supernova/fashionnova/payment/KakaoPayController.java
+++ src/main/java/com/supernova/fashionnova/payment/KakaoPayController.java
@@ -53,7 +53,7 @@ public class KakaoPayController {
     //주문 성공 후 장바구니 비우기 실행
     cartService.clearCart(userId);
     //주문 성공 후 재고 차감
-    productService.calculateQuantity(orderId);
+    productService.calculateQuantity("buy", orderId);
     log.info("결제 성공");
     response.sendRedirect("/payments-completed");
   }
@@ -69,6 +69,8 @@ public class KakaoPayController {
    * */
   @PostMapping("/cancel/{orderId}")
   public KakaoPayCancelResponseDto Cancel(@RequestBody KakaoPayRefundRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long orderId) {
-    return kakaoPayService.kakaoPayCancel(requestDto, userDetails.getUser(), orderId);
+    KakaoPayCancelResponseDto responseDto = kakaoPayService.kakaoPayCancel(requestDto, userDetails.getUser(), orderId);
+    productService.calculateQuantity("cancel", orderId);
+    return responseDto;
   }
 }
