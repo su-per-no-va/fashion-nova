318c2019d9e33cacc20303af60aab913913a117d
Ryujeonggeun <161789810+Ryujeonggeun@users.noreply.github.com>
2024-08-09 13:42:32 +0900

    ♻️ REFACTOR(모든페이지): 로그인 되어있을 때 헤더에 로그 아웃으로 변경, 로그아웃 기능 스토리지 삭제하는걸로 구현

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -11,7 +11,16 @@ $(document).ready(function () {
       type: 'GET',
       url: '/mileages',
       success: function (response) {
-        console.log("사용자 로그인 확인")
+        console.log("사용자 로그인 확인");
+        // 로그인 상태에서 헤더 변경
+        $('#auth-btn').attr("href", "#").text("Logout");
+
+        // 로그아웃 버튼 클릭 시
+        $('#auth-btn').on('click', function (e) {
+          e.preventDefault();
+          localStorage.removeItem('accessToken'); // 토큰 삭제
+          window.location.reload(); // 페이지 리로드
+        });
       },
       error: function (error) {
         alert('로그인 만료, 다시 로그인 해 주세요.');
