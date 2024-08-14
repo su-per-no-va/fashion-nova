17bbfce19dedf5fed8a9c7dfc7f3df59c9533292
equis3351 <equis3351@naver.com>
2024-08-14 12:30:39 +0900

    🐞 FIX(배포): 롤백

--- src/main/java/com/supernova/fashionnova/domain/user/KakaoService.java
+++ src/main/java/com/supernova/fashionnova/domain/user/KakaoService.java
@@ -94,7 +94,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "6a0a695cf674d71866251ddbfd539f84");
-        body.add("redirect_uri", "https://super-nova.store/login.html");
+        body.add("redirect_uri", "https://super-nova.store/users/kakao/callback");
         body.add("code", code);
 
         RequestEntity<MultiValueMap<String, String>> requestEntity = RequestEntity
